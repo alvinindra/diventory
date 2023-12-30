a0ff1dd2b98eaabe32ab086cfe9be987f4ceb04c
Alvin Indra <alvinindrap@gmail.com>
2023-12-30 08:44:49 +0700

    feat: slicing modal detail good

---
+++ components/base/Badge.vue
@@ -0,0 +1,22 @@
+<script setup>
+const props = defineProps({
+  title: {
+    type: String,
+    default: '',
+  },
+  variant: {
+    type: String,
+    default: 'outline',
+  },
+  color: {
+    type: String,
+    default: 'primary',
+  },
+})
+</script>
+
+<template>
+  <div class="border border-solid max-w-min py-[5px] px-2 rounded-lg text-sm" :class="[props.color === 'primary' && 'border-diventory-primary-500 text-diventory-primary-500']">
+    {{ props.title }}
+  </div>
+</template>

c30825581c117572b0d589cffd2cb56313503b60
Alvin Indra <alvinindrap@gmail.com>
2023-12-30 09:32:24 +0700

    feat: slicing stock goods table

--- pages/manage/goods.vue
+++ pages/manage/goods.vue
@@ -9,6 +9,20 @@ useHead({
 definePageMeta({
   layout: 'dashboard',
 })
+
+const listTabs = [
+  {
+    id: 1,
+    name: 'Data Barang',
+    value: 'goods',
+  },
+  {
+    id: 2,
+    name: 'Stok Barang',
+    value: 'stocks',
+  },
+]
+const selectedTab = ref('goods')
 </script>
 
 <template>
@@ -16,16 +30,24 @@ definePageMeta({
     <BaseTitle title="Kelola Barang" />
     <UCard class="mt-5">
       <template #header>
-        <div class="d-flex">
-          <div
-            class="text-base py-3 px-5 text-diventory-primary-600 font-medium border-b-2 border-diventory-primary-600 max-w-fit"
-          >
-            Data Barang
+        <div class="flex flex-row">
+          <div v-for="tab in listTabs" :key="tab" class="d-flex cursor-pointer" @click="selectedTab = tab.value">
+            <div
+              class="text-base py-3 px-5 max-w-fit font-medium text-diventory-black-secondary"
+              :class="[tab.value === selectedTab && 'text-diventory-primary-600 border-b-2 border-diventory-primary-600']"
+            >
+              {{ tab.name }}
+            </div>
           </div>
         </div>
       </template>
 
-      <ManageGoodsData />
+      <template v-if="selectedTab === 'goods'">
+        <ManageGoodsData />
+      </template>
+      <template v-else>
+        <ManageGoodsStocksData />
+      </template>
     </UCard>
   </div>
 </template>

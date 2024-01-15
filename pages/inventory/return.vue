d9161025410d459da840224eac1f8df0e6ddc346
Alvin Indra <alvinindrap@gmail.com>
2024-01-15 22:54:56 +0700

    feat: integrate api return good

--- pages/inventory/return.vue
+++ pages/inventory/return.vue
@@ -14,9 +14,9 @@ const userData = process.client && JSON.parse(window.localStorage.getItem('userD
 const { data: dataGoods } = await useCustomFetch(`/api/pengembalian/${userData?.id}`, { method: 'GET' })
 
 async function returnGood() {
-  const { status, error } = await useCustomFetch('/api/pengembalian', { method: 'PUT', body: {
+  const { status, error } = await useCustomFetch(`/api/pengembalian/`, { method: 'PUT', body: JSON.stringify({
     id: selectedGood.value.id,
-  } })
+  }) })
 
   if (status.value === 'success')
     returnSuccess.value = true
@@ -27,35 +27,40 @@ async function returnGood() {
   }
 }
 
-// function calculateDaysDifference(selectedDate) {
-//   const currentDate = new Date()
+function convertDayToDuration(number) {
+  const years = Math.floor(number / 365)
+  const months = Math.floor((number % 365) / 30)
+  const days = number % 30
 
-//   const selectedDateObject = new Date(selectedDate)
+  let result = ''
 
-//   const timeDifference = selectedDateObject - currentDate
+  if (years > 0)
+    result += `${years} Tahun `
 
-//   const daysDifference = Math.floor(timeDifference / (1000 * 60 * 60 * 24))
-//   return daysDifference
-// }
+  if (months > 0)
+    result += `${months} Bulan `
 
-// function convertDayToDuration(number) {
-//   const years = Math.floor(number / 365)
-//   const months = Math.floor((number % 365) / 30)
-//   const days = number % 30
+  if (days > 0)
+    result += `${days} Hari`
 
-//   let result = ''
+  return result ? result.trim() : '-'
+}
 
-//   if (years > 0)
-//     result += `${years} Tahun `
+function convertDate(dateString) {
+  const dateObj = new Date(dateString)
 
-//   if (months > 0)
-//     result += `${months} Bulan `
+  // Customize the formatting options for Indonesian locale
+  const options = {
+    year: 'numeric',
+    month: 'long',
+    day: 'numeric',
+    locale: 'id-ID', // Specify Indonesian locale
+  }
 
-//   if (days > 0)
-//     result += `${days} Hari`
-
-//   return result ? result.trim() : '-'
-// }
+  // Format the date using toLocaleDateString()
+  const formattedDate = dateObj.toLocaleDateString('id-ID', options)
+  return formattedDate
+}
 </script>
 
 <template>
@@ -103,20 +108,34 @@ async function returnGood() {
                   searchable-placeholder="Cari nama barang yang akan dikembalikan"
                   select-class="w-full px-[14px] py-[10px] mt-2"
                   placeholder="Cari nama barang yang akan dikembalikan"
-                  :options="dataGoods"
-                  option-attribute=""
-                />
+                  :options="dataGoods?.message"
+                >
+                  <template #label>
+                    <span v-if="selectedGood" class="truncate">{{ selectedGood?.id_barang.nama_barang }}</span>
+                    <span v-else>Cari nama barang yang akan dikembalikan</span>
+                  </template>
+                  <template #option="{ option: good }">
+                    <span class="truncate">{{ good.id_barang.nama_barang }}</span>
+                  </template>
+                  <template #trailing>
+                    <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                      <path d="M19.7548 18.394L16.1277 14.7669C17.0009 13.6043 17.4723 12.1893 17.4707 10.7354C17.4707 7.02151 14.4492 4 10.7354 4C7.02151 4 4 7.02151 4 10.7354C4 14.4492 7.02151 17.4707 10.7354 17.4707C12.1893 17.4723 13.6043 17.0009 14.7669 16.1277L18.394 19.7548C18.5776 19.9189 18.8171 20.0065 19.0632 19.9996C19.3094 19.9927 19.5436 19.8919 19.7177 19.7177C19.8919 19.5436 19.9927 19.3094 19.9996 19.0632C20.0065 18.8171 19.9189 18.5776 19.7548 18.394ZM5.92439 10.7354C5.92439 9.78384 6.20655 8.85369 6.73518 8.06253C7.26382 7.27137 8.01519 6.65473 8.89428 6.2906C9.77337 5.92647 10.7407 5.8312 11.6739 6.01683C12.6072 6.20246 13.4644 6.66066 14.1372 7.33349C14.8101 8.00632 15.2683 8.86355 15.4539 9.79679C15.6395 10.73 15.5442 11.6973 15.1801 12.5764C14.816 13.4555 14.1994 14.2069 13.4082 14.7355C12.617 15.2642 11.6869 15.5463 10.7354 15.5463C9.45988 15.5448 8.23708 15.0374 7.33518 14.1355C6.43328 13.2336 5.92592 12.0108 5.92439 10.7354Z" fill="#3A3A3C" fill-opacity="0.4" />
+                    </svg>
+                  </template>
+                </USelectMenu>
               </UFormGroup>
             </ClientOnly>
-            <UFormGroup label="Durasi Peminjaman" name="duration">
-              <UInput model-value="1 Tahun" disabled />
-            </UFormGroup>
-            <UFormGroup label="Tanggal Peminjaman" name="borrow_date">
-              <UInput model-value="8 November 2022" disabled />
-            </UFormGroup>
-            <UFormGroup label="Tanggal Pengembalian" name="return_date">
-              <UInput model-value="8 November 2023" disabled />
-            </UFormGroup>
+            <template v-if="selectedGood">
+              <UFormGroup label="Durasi Peminjaman" name="duration">
+                <UInput :model-value="convertDayToDuration(selectedGood?.durasi_peminjaman)" disabled />
+              </UFormGroup>
+              <UFormGroup label="Tanggal Peminjaman" name="borrow_date">
+                <UInput :model-value="convertDate(selectedGood?.tanggal_peminjaman)" disabled />
+              </UFormGroup>
+              <UFormGroup label="Tanggal Pengembalian" name="return_date">
+                <UInput :model-value="convertDate(selectedGood?.tanggal_pengembalian)" disabled />
+              </UFormGroup>
+            </template>
             <UButton type="submit" class="bg-diventory-primary-600 hover:bg-diventory-primary-500 py-2 justify-center w-full text-center">
               Submit
             </UButton>
